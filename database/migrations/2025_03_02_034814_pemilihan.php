2b6cf16683aa3434ec40068f066c71cef5e9cc17
Mrhehan <muhammadrhehan150@gmail.com>
2025-03-12 20:18:11 +0700

    header user

--- database/migrations/2025_03_02_034814_pemilihan.php
+++ database/migrations/2025_03_02_034814_pemilihan.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->integer('id_user',false,false)->unique();
             $table->integer('id_kandidat',false,false);
             $table->datetime('tanggal');
-            //$table->datetime('tanggal');
             $table->timestamps();
         });
     }
---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,6 @@
+@extends('template.header')
+@section('title', 'Dashboard')
+
+@section('content')
+    <h2>Pasangan Kandidat</h2>
+@endsection
